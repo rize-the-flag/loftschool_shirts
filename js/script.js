f5a29bec2547cf6815e580475034ec3ef6cff208
Denis <dev.denism@yandex.ru>
2020-02-28 02:21:22 +1000

    js.ru styles and classes.

--- js/script.js
+++ js/script.js
@@ -12,7 +12,17 @@ document.addEventListener('DOMContentLoaded', function () {
         init: function(timer){
            this.domTimer = timer;
         },
-        
+
+        getCurrentTime: function () {
+            let now = new Date();
+            return {
+                Hours: now.getHours(),
+                Minutes: now.getMinutes(),
+                Seconds: now.getSeconds()
+            };
+        },
+
+
         go: function (){
             this.timerId = setInterval(()=>{
                 this.updateTimer(this.domTimer);
@@ -22,10 +32,10 @@ document.addEventListener('DOMContentLoaded', function () {
         },
 
         updateTimer: function (){
-            let time = getDateTime();
-            this.hour = time[0] < 10 ? '0' + time[0]:time[0];
-            this.min = time[1] < 10 ? '0' + time[1]:time[1];
-            this.sec = time[2] < 10 ? '0' + time[2]:time[2];
+            let time = this.getCurrentTime();
+            this.hour = time['Hours'] < 10 ? '0' + time['Hours']:time['Hours'];
+            this.min = time['Minutes'] < 10 ? '0' + time['Minutes']:time['Minutes'];
+            this.sec = time['Seconds'] < 10 ? '0' + time['Seconds']:time['Seconds'];
             this.domTimer.querySelector('.hour').firstChild.data = this.hour;
             this.domTimer.querySelector('.minute').firstChild.data =this.min;
             this.domTimer.querySelector('.sec').firstChild.data = this.sec;
@@ -46,6 +56,38 @@ document.addEventListener('DOMContentLoaded', function () {
 
 };
 
+    function showNotification(obj){
+        if (obj) {
+            let div = document.createElement('div');
+
+            obj.top  = obj.top || 0;
+            obj.right = obj.right || 0;
+            obj.className = obj.className || '';
+
+            div.classList.add('notification');
+            div.classList.add(obj.className);
+            div.style.top = obj.top + 'px';
+            div.style.right = obj.right + 'px';
+            div.textContent = obj.html || 'AoaiADsdasda';
+            div.style.position = 'absolute';
+
+            document.body.appendChild(div);
+            return div;
+        }
+    }
+
+
+        let div = showNotification({
+            top: 10, // 10px от верхней границы окна (по умолчанию 0px)
+            right: 10, // 10px от правого края окна (по умолчанию 0px)
+            className: "welcome" // дополнительный класс для div (необязательно)
+        });
+    setTimeout(()=>{
+        div.style.visibility = 'hidden';
+        div.style.opacity = '0';
+    }, 10000);
+
+    /*
     let container = document.querySelector('.timmer');
     wimer.init(container);
     wimer.go();
@@ -54,10 +96,8 @@ document.addEventListener('DOMContentLoaded', function () {
     container.querySelector('.start').addEventListener('click',  ()=>{wimer.start()});
 
 
+    document.getElementById('one').insertAdjacentHTML('afterend', '<li>2</li><li>3</li>');*/
+
 });
 
-function getDateTime() {
-    let now = new Date();
-    return[now.getHours(), now.getMinutes(), now.getSeconds()];
-}
 
