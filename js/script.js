3a3972ba0015fdd874cc9a31a17947bd91b4678a
Denis <dev.denism@yandex.ru>
2020-02-28 02:27:11 +1000

    js.ru scroll task

--- js/script.js
+++ js/script.js
@@ -1,36 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    function showNotification(obj){
-        if (obj) {
-            let div = document.createElement('div');
-
-            obj.top  = obj.top || 0;
-            obj.right = obj.right || 0;
-            obj.className = obj.className || '';
-
-            div.classList.add('notification');
-            div.classList.add(obj.className);
-            div.style.top = obj.top + 'px';
-            div.style.right = obj.right + 'px';
-            div.textContent = obj.html || 'AoaiADsdasda';
-            div.style.position = 'absolute';
-
-            document.body.appendChild(div);
-            return div;
-        }
-    }
-
-
-        let div = showNotification({
-            top: 10, // 10px от верхней границы окна (по умолчанию 0px)
-            right: 10, // 10px от правого края окна (по умолчанию 0px)
-            className: "welcome" // дополнительный класс для div (необязательно)
-        });
-    setTimeout(()=>{
-        div.style.visibility = 'hidden';
-        div.style.opacity = '0';
-    }, 10000);
 
 });
 
