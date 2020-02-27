ee6783f3ecb5fc2c37fd6a4daddbfaec37b09732
Denis <dev.denism@yandex.ru>
2020-02-28 02:23:34 +1000

    js.ru styles and classes.

--- js/script.js
+++ js/script.js
@@ -1,61 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    let wimer = {
-        sec: '00',
-        min: '00',
-        hour: '00',
-        isExecuted: false,
-        timerId: undefined,
-        domTimer: undefined,
-        
-        init: function(timer){
-           this.domTimer = timer;
-        },
-
-        getCurrentTime: function () {
-            let now = new Date();
-            return {
-                Hours: now.getHours(),
-                Minutes: now.getMinutes(),
-                Seconds: now.getSeconds()
-            };
-        },
-
-
-        go: function (){
-            this.timerId = setInterval(()=>{
-                this.updateTimer(this.domTimer);
-            }, 1000);
-            this.isExecuted = !this.isExecuted;
-            this.updateTimer();
-        },
-
-        updateTimer: function (){
-            let time = this.getCurrentTime();
-            this.hour = time['Hours'] < 10 ? '0' + time['Hours']:time['Hours'];
-            this.min = time['Minutes'] < 10 ? '0' + time['Minutes']:time['Minutes'];
-            this.sec = time['Seconds'] < 10 ? '0' + time['Seconds']:time['Seconds'];
-            this.domTimer.querySelector('.hour').firstChild.data = this.hour;
-            this.domTimer.querySelector('.minute').firstChild.data =this.min;
-            this.domTimer.querySelector('.sec').firstChild.data = this.sec;
-        },
-
-        stop: function(){
-            if(this.isExecuted) {
-                clearInterval(this.timerId);
-                this.isExecuted=!this.isExecuted;
-            }
-        },
-
-        start: function () {
-            if(!this.isExecuted) {
-                this.go();
-            }
-        }
-
-};
-
     function showNotification(obj){
         if (obj) {
             let div = document.createElement('div');
@@ -86,17 +31,6 @@ document.addEventListener('DOMContentLoaded', function () {
         div.style.visibility = 'hidden';
         div.style.opacity = '0';
     }, 10000);
-
-    /*
-    let container = document.querySelector('.timmer');
-    wimer.init(container);
-    wimer.go();
-
-    container.querySelector('.stop').addEventListener('click', ()=>{wimer.stop()});
-    container.querySelector('.start').addEventListener('click',  ()=>{wimer.start()});
-
-
-    document.getElementById('one').insertAdjacentHTML('afterend', '<li>2</li><li>3</li>');*/
 
 });
 
